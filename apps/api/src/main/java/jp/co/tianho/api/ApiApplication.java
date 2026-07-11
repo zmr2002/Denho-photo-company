2540b4e8a9d2502750554f351ec5af5eae2de566
zmr2002 <1361419880@qq.com>
2026-07-11 10:19:33 +0900

    fix: disable default API account

--- apps/api/src/main/java/jp/co/tianho/api/ApiApplication.java
+++ apps/api/src/main/java/jp/co/tianho/api/ApiApplication.java
@@ -2,8 +2,9 @@ package jp.co.tianho.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
